a35085214e0d09f294a5e61701e4130a3a89bb01
Carlos Mazon <57258681+reithal@users.noreply.github.com>
2020-01-18 23:40:39 -0500

    completed JavaScript to generate PW Generation

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # PasswordGenerator
+
+## Introduction
+
+Welcome to a Random Password Generator project. This web application is a simple tool that allows the user to select the criteria or requirements of the password, and then randomly select characters from the different subsets and put together a password of the length specified.
+
+## Available Options
+
+The following password parameters are available:
+
+* LowerCase Letters: a - z
+* UpperCase Letters: A-Z
+* Numbers: 0 - 9
+* Special Characters: !?#$%&()*+,-.@/:;<>=[]{}~`|^_
+
+## Contact
+
+Informaiton about the developer can be obtained here.
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,6 @@ var genPassBtn = document.getElementById("generate");
 var resetBtn = document.getElementById("reset");
 var copyButton = document.getElementById("copyBtn")
 
-
 // Grabbing the configuration
 var passLength = document.getElementById("pwLength");
 var upperSwitch = document.getElementById("upperSwitch");
@@ -15,8 +14,7 @@ var specialSwitch = document.getElementById("specialSwitch");
 var pwLengthOutput = document.getElementById("pwLengthOutput");
 var passwordValue = document.getElementById("passwordValue");
 
-// storing the password
-var password = "";
+
 
 //arrays of the characters to use for password generation
 var lowerLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
@@ -24,7 +22,6 @@ var upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", 
 // Storing as strings in case 2 numbers are randomly selected first and the system adds them.
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialCharacters = ["!","?", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "@", "/", ":", ";", "<", ">", "=", "[" , "]", "{", "}", "~", "`", "|", "^", "_" ];
-
 
 //display the default value of the slider
 pwLengthOutput.innerHTML = passLength.value;
@@ -58,17 +55,37 @@ var getRandomElement=function(array) {
 };
 
 
-
 function generatePassword(arr) {
+  // storing the password
+  var password = "";
   var options = checkConfig();
   if (options.length == 0)
   {
     alert("Please select at least one character option.");
     return;
   }
-  getRandomElement(options);
 
+  for (i=0; i<passLength.value; i++) {
+    var type = getRandomElement(options);
+   
+    if(type == "u")
+    {
+      password = password + getRandomElement(upperLetters);
+    }
+    else if (type == "l") {
+      password = password + getRandomElement(lowerLetters);
+    }
+    else if (type == "n") {
+      password = password + getRandomElement(numbers);
+    }
+    else if (type == "s") {
+      password = password + getRandomElement(specialCharacters);
+    }
+  }
+  console.log(password);
+  passwordValue.value=password;
 }
+
 
 function copyPassword() {
   passwordValue.select();
